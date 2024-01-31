31d8297020380a4d27142ebebc89082b3da87532
Tuusa68 <157683761+Tuusa68@users.noreply.github.com>
2024-01-31 12:22:26 +0200

    Update omapeli.py
    Tein score systeemin
    Peli loppuu kolmeen maaliin -> nukkuu kaks sekunttia ja näyttää voittajan näytöllä ja sulkee pelin

--- vko4 peli/omapeli.py
+++ vko4 peli/omapeli.py
@@ -2,12 +2,15 @@ import pygame
 import sys
 from pygame.locals import *
 pygame.init()
+import time
 
 width = 1200
 height = 750
 dispSurf = pygame.display.set_mode((width,height))
 pygame.display.set_caption("jutix1 futispeli")
 
+font = pygame.font.Font(None, 36)
+font2 = pygame.font.Font(None, 100)
 footballpitch = pygame.image.load("footballpitch.jpg").convert()
 player1 = pygame.image.load("player1.jpg").convert()
 player2 = pygame.image.load("player2.jpg").convert()
@@ -29,10 +32,10 @@ rectangle2.fill(red)
 
 
 dispSurf.blit(footballpitch, (0,0))
-dispSurf.blit(maali, (0,0))
-dispSurf.blit(goal, (0,0))
 dispSurf.blit(player1, (0,0))
 dispSurf.blit(player2, (0,0))
+dispSurf.blit(maali, (0,0))
+dispSurf.blit(goal, (0,0))
 dispSurf.blit(ball, (0,0))
 dispSurf.blit(rectangle, (0,0))
 dispSurf.blit(rectangle2, (0,0))
@@ -61,8 +64,8 @@ rectangle2Area.top = 510
 
 
 speed = [2,2]
-
-
+score = 0
+score2 = 0
 
 while True:
 
@@ -72,8 +75,9 @@ while True:
             sys.exit()
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
-                pygame.guit()
+                pygame.quit()
                 sys.exit()
+            
 
     ballArea.move_ip(speed)
     
@@ -106,30 +110,28 @@ while True:
             speed[1] = -speed[1]
         else:
             speed[0] = -speed[0]
-   
 
     if rectangleArea.colliderect(ballArea):
-        dispSurf.blit(footballpitch, (0,0))
-        largeFont = pygame.font.SysFont('comicsans', 80)
-        currentScore = largeFont.render('Player1 scored:'+ str(score),200,(white))
-        dispSurf.blit(currentScore, (width/2 - currentScore.get_width()/2, 240))
-        pygame.display.flip()
-    score = 0
+        if rectangleArea.colliderect(ballArea.move(-speed[0],0)):
+            speed[1] = -speed[1]
+        else:
+            speed[0] = -speed[0]
+    if rectangle2Area.colliderect(ballArea):
+        if rectangle2Area.colliderect(ballArea.move(-speed[0],0)):
+            speed[1] = -speed[1]
+        else:
+            speed[0] = -speed[0]
+   
+    if rectangleArea.colliderect(ballArea):
+        ballArea.left = 600
+        ballArea.top = 375
+        score += 1
 
     if rectangle2Area.colliderect(ballArea):
-        dispSurf.blit(footballpitch, (0,0))
-        largeFont = pygame.font.SysFont('comicsans', 80)
-        currentScore2 = largeFont.render('Player2 scored:'+ str(score2),200,(white))
-        dispSurf.blit(currentScore2, (width/2 - currentScore2.get_width()/2, 240))
-        pygame.display.flip()
-    score2 = 0
+        ballArea.left = 600
+        ballArea.top = 375
+        score2 += 1
 
-
-
-
-
-
-   
     pressings2 = pygame.key.get_pressed()
     if pressings2[K_a]:
         player2Area.move_ip((-1,0))
@@ -150,8 +152,22 @@ while True:
     if pressings1[K_UP]:
         player1Area.move_ip((0,-1))
 
+    if score == 3:
+        player1_win_text = font2.render(f"Player 1 WINNER:", True, (255, 255, 255))
+        dispSurf.blit(player1_win_text, (300, 300))
+        pygame.display.flip()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
 
-
+    if score2 == 3:
+        player2_win_text = font2.render(f"Player 2 WINNER", True, (255, 255, 255))
+        dispSurf.blit(player2_win_text, (300, 300))
+        pygame.display.flip()
+        time.sleep(2)
+        pygame.quit()
+        sys.exit()
+    
     dispSurf.blit(footballpitch, (0,0))
     dispSurf.blit(maali, maaliArea)
     dispSurf.blit(goal, goalArea)
@@ -160,6 +176,10 @@ while True:
     dispSurf.blit(ball, ballArea)
     dispSurf.blit(rectangle, rectangleArea)
     dispSurf.blit(rectangle2, rectangle2Area)
+    score_text = font.render(f"Player 1:  {score}", True, (255, 255, 255))
+    score2_text = font.render(f"Player 2:  {score2}", True, (255, 255, 255))
+    dispSurf.blit(score2_text, (600, 25))
+    dispSurf.blit(score_text, (400, 25))
 
 
 
